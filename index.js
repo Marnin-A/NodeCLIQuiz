ccfdfe43684e8df9422419569eb5c7cda4e89d8e
Marnin Audu <msmaudu@gmail.com>
2022-10-07 16:36:10 +0100

    Created an async funtion to show text to user, and helper function that causes delay so the user can view it

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ import gradient from "gradient-string";
 import chalkAnimation from "chalk-animation";
 import figlet from "figlet";
 import { createSpinner } from "nanospinner";
+
+console.log(chalk.bgMagentaBright("Sup gee"));
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+async function welcome() {
+  const rainbowTitle = chalkAnimation.rainbow(
+    "Who wants to be a Javascript Millionaire? \n"
+  );
+}
